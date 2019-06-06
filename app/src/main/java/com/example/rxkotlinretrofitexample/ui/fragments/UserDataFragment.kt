132fbcecbea16977f8b3f8df6cc7d1f02643de2f
Takari Harrison <takariharrison@gmail.com>
2019-06-06 19:36:16 -0400

    Rename RecyclerViewFragment to UserDataFragment and make viewModel private

--- app/src/main/java/com/example/rxkotlinretrofitexample/ui/fragments/UserDataFragment.kt
+++ app/src/main/java/com/example/rxkotlinretrofitexample/ui/fragments/UserDataFragment.kt
@@ -2,36 +2,33 @@ package com.example.rxkotlinretrofitexample.ui.fragments
 
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rxkotlinretrofitexample.R
 import com.example.rxkotlinretrofitexample.data.remote.response.UserData
-import com.example.rxkotlinretrofitexample.ui.MyAdapter
-import com.example.rxkotlinretrofitexample.viewmodel.ViewModel
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import com.example.rxkotlinretrofitexample.ui.UserDataAdapter
+import com.example.rxkotlinretrofitexample.viewmodel.UserDataViewModel
 import kotlinx.android.synthetic.main.fragment_recycler_view.*
 
 
-class RecyclerViewFragment : Fragment() {
+class UserDataFragment : Fragment() {
+
+    private val viewModel by activityViewModels<UserDataViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_recycler_view, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val viewModel = ViewModelProviders.of(activity!!).get(ViewModel::class.java)
 
         viewModel.getNetworkCallResults().observe(this, Observer { userData -> initRecyclerView(userData) })
     }
@@ -40,7 +37,7 @@ class RecyclerViewFragment : Fragment() {
         recyclerView.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
-            adapter = MyAdapter(userData, context)
+            adapter = UserDataAdapter(userData, context)
         }
     }
 }
