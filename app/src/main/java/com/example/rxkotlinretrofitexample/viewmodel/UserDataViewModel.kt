d3375eb51316a95afb56c492ef2ed0f93ba833b9
Takari Harrison <takariharrison@gmail.com>
2019-06-06 19:18:52 -0400

    Rename ViewModel to UserDataViewModel, rename some properties and remove redundant disposable

---
+++ app/src/main/java/com/example/rxkotlinretrofitexample/viewmodel/UserDataViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.rxkotlinretrofitexample.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.rxkotlinretrofitexample.data.Repository
+import com.example.rxkotlinretrofitexample.data.remote.api.JsonPlaceHolderApi
+import com.example.rxkotlinretrofitexample.data.remote.response.UserData
+import io.reactivex.rxkotlin.subscribeBy
+
+
+class UserDataViewModel : ViewModel() {
+
+    private val repository = Repository(JsonPlaceHolderApi.invoke())
+    private val userDataList = MutableLiveData<List<UserData>>()
+    private val networkCallSuccess = MutableLiveData<Boolean>()
+
+    fun executeNetworkCall() =
+        repository.executeNetworkCall()
+            .subscribeBy(
+                onSuccess = { userData ->
+                    userDataList.postValue(userData)
+                    networkCallSuccess.value = true
+                },
+                onError = { networkCallSuccess.value = false })
+
+    fun getNetworkCallSuccess(): LiveData<Boolean> = networkCallSuccess
+
+    fun getNetworkCallResults(): LiveData<List<UserData>> = userDataList
+}
