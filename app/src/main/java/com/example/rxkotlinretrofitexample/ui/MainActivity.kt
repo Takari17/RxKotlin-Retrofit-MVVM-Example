c5de1984ca1ea7fac0e9037aa7e98a23c9173f2e
Takari Harrison <takariharrison@gmail.com>
2019-06-06 19:34:25 -0400

    Clean up code in MainActivity
    Fixed some repetitive unnecessary code and shortened some stuff.

--- app/src/main/java/com/example/rxkotlinretrofitexample/ui/MainActivity.kt
+++ app/src/main/java/com/example/rxkotlinretrofitexample/ui/MainActivity.kt
@@ -1,49 +1,49 @@
 package com.example.rxkotlinretrofitexample.ui
 
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.rxkotlinretrofitexample.R
 import com.example.rxkotlinretrofitexample.ui.fragments.FailedFragment
 import com.example.rxkotlinretrofitexample.ui.fragments.ProgressBarFragment
 import com.example.rxkotlinretrofitexample.ui.fragments.RecyclerViewFragment
-import com.example.rxkotlinretrofitexample.viewmodel.ViewModel
+import com.example.rxkotlinretrofitexample.viewmodel.UserDataViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<UserDataViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        showProgressFragment() // shows progress bar initially until network call result
+        // shows progress bar initially until network call result
+        showProgressFragment()
 
-        val viewModel = ViewModelProviders.of(this).get(ViewModel::class.java)
-
-        viewModel.getNetworkCallSuccess().observe(this, Observer { success ->  // emits on network call result
-            if(success) showRecyclerViewFragment() else showFailedFragment() })
+        viewModel.getNetworkCallSuccess().observe(this, Observer { success ->
+            if (success) showRecyclerViewFragment() else showFailedFragment()
+        })
 
         viewModel.executeNetworkCall()
     }
 
-    private fun showRecyclerViewFragment() {
-        val recyclerViewFragment = RecyclerViewFragment()
-        replaceContainer(recyclerViewFragment)
-    }
+    private fun showRecyclerViewFragment() =
+        replaceContainer(RecyclerViewFragment())
 
-    private fun showFailedFragment() {
-    val failedFragment = FailedFragment()
-        replaceContainer(failedFragment)
-    }
 
-    private fun showProgressFragment() {
-        val progressFragment = ProgressBarFragment()
-        replaceContainer(progressFragment)
-    }
+    private fun showFailedFragment() =
+        replaceContainer(FailedFragment())
 
-    private fun replaceContainer(fragment: Fragment){
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
-    }
+
+    private fun showProgressFragment() =
+        replaceContainer(ProgressBarFragment())
+
+
+    private fun replaceContainer(fragment: Fragment) =
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
 }
