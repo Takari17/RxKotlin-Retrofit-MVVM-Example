2598c4eb6586b4a19ecb6e8f23ff919f0ce99b99
Takari Harrison <takariharrison@gmail.com>
2019-06-06 19:51:03 -0400

    Remove unneeded UserData properties

--- app/src/main/java/com/example/rxkotlinretrofitexample/data/remote/response/UserData.kt
+++ app/src/main/java/com/example/rxkotlinretrofitexample/data/remote/response/UserData.kt
@@ -1,8 +1,7 @@
 package com.example.rxkotlinretrofitexample.data.remote.response
 
-//todo check if you use all of these properties
+
 data class UserData(
-    val address: Address,
     val company: Company,
     val email: String,
     val id: Int,
@@ -12,21 +11,7 @@ data class UserData(
     val website: String
 )
 
-data class Address(
-    val city: String,
-    val geo: Geo,
-    val street: String,
-    val suite: String,
-    val zipcode: String
-)
-
 data class Company(
-    val bs: String,
     val catchPhrase: String,
     val name: String
-)
-
-data class Geo(
-    val lat: String,
-    val lng: String
 )
