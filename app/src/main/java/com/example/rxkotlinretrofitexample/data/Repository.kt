bb29b9c7e35047b54de8f94e17126664a06c8198
Takari Harrison <takariharrison@gmail.com>
2019-06-06 19:15:53 -0400

    Inject the Repository's dependencies and remove redundant observeOn call

--- app/src/main/java/com/example/rxkotlinretrofitexample/data/Repository.kt
+++ app/src/main/java/com/example/rxkotlinretrofitexample/data/Repository.kt
@@ -3,17 +3,12 @@ package com.example.rxkotlinretrofitexample.data
 import com.example.rxkotlinretrofitexample.data.remote.api.JsonPlaceHolderApi
 import com.example.rxkotlinretrofitexample.data.remote.response.UserData
 import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 
-class Repository {
+class Repository(private val api: JsonPlaceHolderApi) {
 
-    private val api = JsonPlaceHolderApi.invoke()
-
-    fun executeNetworkCall(): Single<List<UserData>> {
-        return api.getAllUserData()
+    fun executeNetworkCall(): Single<List<UserData>> =
+        api.getAllUserData()
             .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-    }
 }
