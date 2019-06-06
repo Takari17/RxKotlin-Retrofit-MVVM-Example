0d752919d095152fb34d6119df6b28335da87fb5
Takari Harrison <takariharrison@gmail.com>
2019-06-06 19:59:24 -0400

    Make the Retrofit's instance a singleton and move the constant Url values into JsonPlaceHolderApi's companion object

--- app/src/main/java/com/example/rxkotlinretrofitexample/data/remote/request/JsonPlaceHolderApi.kt
+++ app/src/main/java/com/example/rxkotlinretrofitexample/data/remote/request/JsonPlaceHolderApi.kt
@@ -7,25 +7,31 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-
-const val BASE_URL = "https://jsonplaceholder.typicode.com/"
-const val API = "users"
-
 interface JsonPlaceHolderApi {
+
 
     @GET(API)
     fun getAllUserData(): Single<List<UserData>>
 
 
-    companion object{
+    companion object {
+
+        const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+        const val API = "users"
+
+        private var api: JsonPlaceHolderApi? = null
+
         operator fun invoke(): JsonPlaceHolderApi {
 
-            return  Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(JsonPlaceHolderApi::class.java)
+            if (api == null) {
+                api = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(JsonPlaceHolderApi::class.java)
+            }
+            return api!!
         }
     }
 }
